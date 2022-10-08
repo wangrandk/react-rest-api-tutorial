5c0aa4db0ee60dc1aa4dbb500bb4a615b3a1fa43
RAN <wangrandk@users.noreply.github.com>
2022-10-08 20:11:32 +0200

    done swr

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,28 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
-import Contact from "./Contact";
+import useSWR from 'swr'
+import Contact from './Contact'
+
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 function App() {
-  const [contacts, setContacts] = useState([]);
-  const [error, setError] = useState(null);
+  const{data,error} = useSWR(
+    "https://jsonplaceholder.typicode.com/users",
+    fetcher
+  );
 
-  useEffect(() => {
-    axios("https://jsonplaceholder.typicode.com/users")
-      .then((response) => {
-        setError(null);
-        setContacts(response.data);
-      })
-      .catch(setError);
-
-    /* Using Fetch
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((response) => response.json())
-      .then((response) => {
-        setContacts(response);
-        setError(null);
-      })
-      .catch(setError);
-    */
-  }, []);
-
-  if (error) return <p>An error occurred</p>;
-
+  if(error) return <p>An error occurred</p>;
+  if(!data) return <p>Loading</p>;
   return (
-    <div className="App">
-      {contacts.map(({ id, name, email, company }) => (
-        <Contact
-          key={id}
-          name={name}
-          email={email}
-          tagline={company.catchPhrase}
-        />
-      ))}
+    <div className='App'>
+      {
+        data.map(({id,name,email,company}) => (
+          <Contact
+            key={id}
+            name={name}
+            email={email}
+            tagline={company.catchPhrase}
+            />
+        ))
+      }
     </div>
   );
 }
